80254c9d43525e8c34f1df6ad0734a11892af247
JaimeTAR <jaimetadeoalfaro@hotmail.com>
2025-01-29 18:17:42 -0600

    feature: signup page without styling

--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -1,13 +1,127 @@
-import './Signup.css'
+import "./Signup.css";
+import { useState } from "react";
 
-function Signup() {
-  return (
-    <>
-      <h1>Pokemon Battle Arena</h1>
-      <h2>Sign Up Page</h2>
-    </>
-
-  )
+interface FormData {
+  email: string;
+  password: string;
+  confirmedPassword: string;
 }
 
-export default Signup
+interface ValidPass {
+  length: boolean;
+  caps: boolean;
+  numbers: boolean;
+  special: boolean;
+}
+
+interface PostFormData {
+  email: string;
+  password: string;
+}
+
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+function Signup() {
+  const [formData, setFormData] = useState<FormData>({
+    email: "",
+    password: "",
+    confirmedPassword: "",
+  });
+
+  const [validEmailText, setValidEmailText] = useState<boolean>(false);
+  const [validEmail, setValidEmail] = useState<boolean>(false);
+
+  const [validPassText, setValidPassText] = useState<boolean>(false);
+  const [validPass, setValidPass] = useState<boolean>(false);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    setFormData((prev) => {
+      const updatedFormData = { ...prev, [name]: value };
+
+      // if (updatedFormData.password && updatedFormData.confirmedPassword) {
+      //   setConfirmedPassText(true);
+      //   setConfirmedPass(
+      //     updatedFormData.password === updatedFormData.confirmedPassword
+      //   );
+      // } else {
+      //   setConfirmedPassText(false);
+      // }
+
+      if (updatedFormData.email) {
+        if (emailRegex.test(updatedFormData.email)) {
+          setValidEmailText(false);
+          setValidEmail(true);
+        } else {
+          setValidEmailText(true);
+          setValidEmail(false);
+        }
+      } else {
+        setValidEmailText(false);
+        setValidEmail(false);
+      }
+
+      return updatedFormData;
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (validEmail) {
+      const postData: PostFormData = {
+        email: formData.email,
+        password: formData.password,
+      };
+      console.log("Submitted Data:", postData);
+    } else {
+      console.log("not valid data");
+    }
+  };
+
+  return (
+    <div className="signup-container">
+      <h1>Pokemon Battle Arena</h1>
+      <h3>Create account</h3>
+      <div className="form">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">E-mail</label>
+          <input
+            type="text"
+            name="email"
+            placeholder="E-mail"
+            value={formData.email}
+            onChange={handleChange}
+          />
+          {validEmailText && <p>Please write a valid email adress</p>}
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            name="confirmedPassword"
+            placeholder="Confirm password"
+            value={formData.confirmedPassword}
+            onChange={handleChange}
+          />
+          {/* {confirmedPassText && (
+            <p>
+              {confirmedPass
+                ? "Las contraseñas coinciden"
+                : "Las contraseñas no coinciden"}
+            </p>
+          )} */}
+          <input className="submit-btn" type="submit" value="Sign up" />
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
